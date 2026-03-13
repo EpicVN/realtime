2f88fa4788d2af23d3a6f84128410a188b4fdc96
EpicVN <ledangkhoahi3@gmail.com>
2026-03-13 14:51:34 +0700

    update: update logo ui for docker

--- components/Helper/Logo.tsx
+++ components/Helper/Logo.tsx
@@ -1,13 +1,10 @@
 import Image from "next/image";
 
-// Định nghĩa kiểu dữ liệu cho props
 interface LogoProps {
-  type?: "white" | "black"; // type là tùy chọn, mặc định sẽ là 'white' hoặc 'black' tùy bạn set
+  type?: "white" | "black";
 }
 
 const Logo = ({ type = "white" }: LogoProps) => {
-  // Chọn đường dẫn ảnh dựa trên type
-  // Giả sử bạn có 2 file: logo-white.png và logo-black.png trong thư mục public/images
   const src =
     type === "white" ? "/images/logo.png" : "/images/logo_bg_white.png";
 
@@ -18,8 +15,9 @@ const Logo = ({ type = "white" }: LogoProps) => {
         alt="Realtime Solutions Logo"
         width={200}
         height={60}
-        className="w-48 h-auto md:w-auto md:h-auto object-contain transition-opacity duration-300"
-        priority // Logo nên được ưu tiên tải trước
+        className="w-56 md:w-72 h-auto object-contain transition-opacity duration-300"
+        priority
+        unoptimized={true}
       />
     </div>
   );
