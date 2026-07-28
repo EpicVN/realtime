13e1f565aad3282b368850643b59cc006d471700
EpicVN <ledangkhoahi3@gmail.com>
2026-07-28 13:12:10 +0700

    update: api tts

--- app/actions/tts.ts
+++ app/actions/tts.ts
@@ -24,7 +24,8 @@ export async function createSpeech(formData: FormData) {
     // 2. Cấu hình gửi POST
     //const apiUrl = "http://127.0.0.1:8086/api/tts-create-speak.php";
     //const apiUrl = "http://109.237.69.136:8086/api/tts-create-speak.php";
-    const apiUrl = "http://103.154.176.65:8086/api/tts-create-speak.php";
+    // const apiUrl = "http://103.154.176.65:8086/api/tts-create-speak.php";
+    const apiUrl = "http://103.229.42.19:8086/api/tts-create-speak.php"; // s10
 
     console.log("🚀 Đang gửi POST tới:", apiUrl);
     console.log("📦 Payload:", JSON.stringify(payload));
